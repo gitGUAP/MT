e389b881194718da1c353bf4fdc338b4dc1a7c03
Sergey <grishy.ru@yandex.ru>
2018-02-27 20:10:38 +0300

    fix tabel

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@ map<string, int> table = {
 	{ "S2", 1 },{ "S3", 1 },{ "S4", 1 },{ "Sx", 1 },
 	{ "E2", 2 },{ "E3", 2 },{ "E4", 2 },{ "Ex", 2 },
 	{ "T2", 5 },{ "T3", 5 },{ "T4", 5 },{ "Tx", 10 },
-	{ "Z+", 5 },{ "Z4", 4 },
+	{ "Z+", 3 },{ "Z$", 4 },
 	{ "K2", 11 },{ "K3", 12 },{ "K4", 13 },
-	{ "Y*", 6 },{ "Y$", 7 },
-	{ "W^", 8 },{ "W+", 9 },{ "W$", 9 }
+	{ "Y*", 6 },{ "Y+", 7 },{ "Y$", 7 },
+	{ "W^", 8 },{ "W+", 9 },{ "W$", 9 },
+	{ "$$", 0 }
 };
 
 
@@ -32,39 +33,62 @@ string grammar[] = { "Blanket",
 };
 
 bool check(string inp) {
-	string stack = "#S";
-	inp += '#';
+	string stack = "$S";
+	inp += '$';
 
-	//for (int i = 1; !inp.empty(); i++) {
-	//	char symb = inp.front();
-	//	char stackState = stack.back();
-	//	int indexT = tableTop.find(symb);
-	//	int indexL = tableLeft.find(stackState);
+	for (int i = 1; !inp.empty(); i++) {
+		char symb = inp.front();
+		char stackState = stack.back();
 
-	//	if (indexT == std::string::npos)
-	//		return false;
+		if (symb == stackState) {
+			if (symb == '$')
+				return true;
 
-	//	int command = table[indexL][indexT];
+			inp.erase(0, 1);
+			stack.pop_back();
+			continue;
+		}
 
-	//	switch (command) {
-	//	case 0: return true;
-	//	case -2: return false;
-	//	case -1: {
-	//		inp.erase(0, 1);
-	//		stack.pop_back();
-	//		continue;
-	//	}
-	//	}
+		auto finded = table.find(string({ stackState ,symb }));
 
-	//	string state = grammar[command];
-	//	stack.pop_back();
-	//	stack += string(state.rbegin(), state.rend());;
+		if (finded == table.end())
+			return false;
+		else {
+			string state = grammar[finded->second];
 
-	//	cout << "\t" << "--- " << i << " ---" << endl
-	//		<< "String: " << inp << endl
-	//		<< "Command: " << command << endl
-	//		<< "Stack: " << stack << endl;
-	//}
+			stack.pop_back();
+			stack += string(state.rbegin(), state.rend());
+		}
+	
+
+
+		//	int indexT = tableTop.find(symb);
+		//	int indexL = tableLeft.find(stackState);
+
+		//	if (indexT == std::string::npos)
+		//		return false;
+
+		//	int command = table[indexL][indexT];
+
+		//	switch (command) {
+		//	case 0: return true;
+		//	case -2: return false;
+		//	case -1: {
+		//		inp.erase(0, 1);
+		//		stack.pop_back();
+		//		continue;
+		//	}
+		//	}
+
+		//	string state = grammar[command];
+		//	stack.pop_back();
+		//	stack += string(state.rbegin(), state.rend());;
+
+		//	cout << "\t" << "--- " << i << " ---" << endl
+		//		<< "String: " << inp << endl
+		//		<< "Command: " << command << endl
+		//		<< "Stack: " << stack << endl;
+	}
 	return false;
 }
 
